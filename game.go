bd0946e22e5b94132680244130ce16ea3f098684
Pranav Madhu <pranavmadhu2003@gmail.com>
2025-10-23 19:43:08 +0530

    Implements code to get count of alive neighbours at right edges

--- game.go
+++ game.go
@@ -82,7 +82,9 @@ func countAliveNeighbours(grid Grid, x uint, y uint) int {
 	if y == 0 { //left edge
 		return b2i(grid.data[x-1][y]) + b2i(grid.data[x-1][y+1]) + b2i(grid.data[x][y+1]) + b2i(grid.data[x+1][y+1]) + b2i(grid.data[x+1][y])
 	}
-
+	if y == grid.size-1 { //right edge
+		return b2i(grid.data[x-1][y]) + b2i(grid.data[x+1][y]) + b2i(grid.data[x-1][y-1]) + b2i(grid.data[x][y-1]) + b2i(grid.data[x+1][y-1])
+	}
 	return 0
 }
 
