02aebf431de5ab572ce77568b854401242613165
Pranav Madhu <pranavmadhu2003@gmail.com>
2025-10-23 16:24:41 +0530

    Adds test case to check the count of alive neighbours of bottom right corner

--- game_test.go
+++ game_test.go
@@ -105,3 +105,23 @@ func TestCountAliveNeighbourBottomLeft(t *testing.T) {
 		t.Errorf("Expected neighbour count to be 3, but got %d", actual)
 	}
 }
+
+func TestCountAliveNeighbourBottomRight(t *testing.T) {
+	grid := newGrid(5, 4, 3)
+	actual := countAliveNeighbours(grid, 4, 4)
+
+	if actual != 1 {
+		t.Errorf("Expected neighbour count to be 1, but got %d", actual)
+	}
+	grid = newGrid(5, 4, 3, 3, 3)
+	actual = countAliveNeighbours(grid, 4, 4)
+	if actual != 2 {
+		t.Errorf("Expected neighbour count to be 2, but got %d", actual)
+	}
+
+	grid = newGrid(5, 4, 3, 3, 3, 3, 4)
+	actual = countAliveNeighbours(grid, 4, 4)
+	if actual != 3 {
+		t.Errorf("Expected neighbour count to be 3, but got %d", actual)
+	}
+}
